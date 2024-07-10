398fbf1f760a573284433d4d57b856ec77f6a8eb
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-07-10 10:48:40 +0000

    BASIC AUTHENTICATION

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,18 +6,19 @@ from typing import (
     TypeVar
 )
 
+
 class Auth:
     """Auth class"""
-    def  require_auth(self,
-                      path: str,
-                      excluded_paths: List[str]) -> bool:
+    def require_auth(self,
+                     path: str,
+                     excluded_paths: List[str]) -> bool:
         """
         A public method to validate if a url requires an authentication
 
         Return: False or True
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """
         A public method that checks the authorization header
