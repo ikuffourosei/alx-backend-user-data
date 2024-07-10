8a43584d951be4defb8683f841b574ff01ca04b8
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-07-10 10:46:28 +0000

    BASIC AUTHENTICATION

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Authentication class"""
+from flask import request
+from typing import (
+    List,
+    TypeVar
+)
+
+class Auth:
+    """Auth class"""
+    def  require_auth(self,
+                      path: str,
+                      excluded_paths: List[str]) -> bool:
+        """
+        A public method to validate if a url requires an authentication
+
+        Return: False or True
+        """
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """
+        A public method that checks the authorization header
+        """
+        return request
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """A public method that checks for current user"""
+        return request
