965bf4d03e992c591b74dda8ea30499b88166d48
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-07-11 17:02:11 +0000

    BASIC AUTHENTICATION

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,11 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """A Basic Auth class that inherits from Auth"""
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """A public method to extract base64 header"""
+        if isinstance(authorization_header, str):
+            val = authorization_header.split()
+            if len(val) > 1 and val[0] == 'Basic':
+                return val[1]
+        return None
