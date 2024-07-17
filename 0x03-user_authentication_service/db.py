f1016d0f12696c6b9a3eec20c3501e27aa0b01e8
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-07-17 20:38:21 +0000

    User Authentication Service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,10 +34,10 @@ class DB:
         """Adds a user to the database
         """
         try:
-            Newuser = User(email=email, hashed_password=hashed_password)
-            self._session.add(Newuser)
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
             self._session.commit()
         except Exception:
             self._session.rollback()
-            Newuser = None
-        return Newuser
+            new_user = None
+        return new_user
